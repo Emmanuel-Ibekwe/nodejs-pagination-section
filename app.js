abdcf89f0d0391cc239d88acec85c062b57c4856
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2023-12-19 13:48:52 +0100

    enabled ssl/tls

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
+
 const path = require("path");
+const fs = require("fs");
+const https = require("https");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -6,6 +10,9 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
 
 const multer = require("multer");
 const flash = require("connect-flash");
@@ -15,8 +22,9 @@ const flash = require("connect-flash");
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
-const MONGODB_URI =
-  "mongodb+srv://ibekweemmanuel007:(STARtrek1999)@cluster0.5aiiow7.mongodb.net/shop";
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.5aiiow7.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
+console.log(process.env.NODE_ENV);
 
 const app = express();
 const store = new MongoDBStore({
@@ -25,6 +33,9 @@ const store = new MongoDBStore({
 });
 
 const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync("server.key");
+const certificate = fs.readFileSync("server.cert");
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -55,6 +66,14 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
@@ -122,7 +141,10 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
+    // https
+    //   .createServer({ key: privateKey, cert: certificate }, app)
+    //   .listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
